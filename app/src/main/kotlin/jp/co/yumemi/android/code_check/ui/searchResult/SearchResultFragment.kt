8f52422fe9c200f4fa46b42b6c42b79ff00d40b9
SuzakiShouta <k18054kk@gmail.com>
2023-03-02 13:27:32 +0900

    Move SearchResultFragment Base to ui.searchResult

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/searchResult/SearchResultFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/searchResult/SearchResultFragment.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.code_check
+package jp.co.yumemi.android.code_check.ui.searchResult
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,7 +12,10 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
+import jp.co.yumemi.android.code_check.OneViewModel
+import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.FragmentSearchResultBinding
+import jp.co.yumemi.android.code_check.item
 
 class SearchResultFragment: Fragment(R.layout.fragment_search_result){
 
