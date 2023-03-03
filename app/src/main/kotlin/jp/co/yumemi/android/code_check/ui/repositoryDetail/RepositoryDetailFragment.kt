3273c1d9beec4d3c66bb0924bc6fd8bbc7d0837c
SuzakiShouta <k18054kk@gmail.com>
2023-03-03 12:29:34 +0900

    Fix ViewBindingをonCreateViewでバインドするように変更
    3f01ec4をRepositoryDetailFragmentでも行う

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/repositoryDetail/RepositoryDetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/repositoryDetail/RepositoryDetailFragment.kt
@@ -5,13 +5,16 @@ package jp.co.yumemi.android.code_check.ui.repositoryDetail
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
 import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.code_check.databinding.FragmentRepositoryDetailBinding
+import jp.co.yumemi.android.code_check.databinding.FragmentSearchResultBinding
 import jp.co.yumemi.android.code_check.model.Repository
 
 class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
@@ -20,6 +23,14 @@ class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
 
     private var _binding: FragmentRepositoryDetailBinding? = null
     private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRepositoryDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
