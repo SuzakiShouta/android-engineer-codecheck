60b0e95f401420b4c12190aa3cc76a874a83333d
SuzakiShouta <k18054kk@gmail.com>
2023-03-08 15:45:23 +0900

    Change SearchResultViewModelがGithubApiクラスを使用するように変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/searchResult/SearchResultViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/searchResult/SearchResultViewModel.kt
@@ -16,6 +16,7 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import jp.co.yumemi.android.code_check.MainApplication
 import jp.co.yumemi.android.code_check.R
+import jp.co.yumemi.android.code_check.api.GithubApi
 import jp.co.yumemi.android.code_check.model.Repository
 import kotlinx.coroutines.launch
 import org.json.JSONArray
@@ -31,40 +32,7 @@ class SearchResultViewModel(val app: MainApplication) : ViewModel() {
     // githubAPIを叩き、検索結果をLiveDataに投げる
     fun searchResults(inputText: String) {
         viewModelScope.launch {
-            val client = HttpClient(Android)
-
-            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-                header("Accept", "application/vnd.github.v3+json")
-                parameter("q", inputText)
-            }
-
-            val jsonBody = JSONObject(response.receive<String>())
-            val jsonItems = jsonBody.optJSONArray("items") ?: JSONArray()
-            val repositories = mutableListOf<Repository>()
-
-            for (i in 0 until jsonItems.length()) {
-                val jsonItem = jsonItems.optJSONObject(i)
-                val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")?.optString("avatar_url") ?: ""
-                val language = jsonItem.optString("language")
-                val stargazersCount = jsonItem.optLong("stargazers_count")
-                val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_count")
-                val openIssuesCount = jsonItem.optLong("open_issues_count")
-
-                repositories.add(
-                    Repository(
-                        name = name,
-                        ownerIconUrl = ownerIconUrl,
-                        language = app.applicationContext.getString(R.string.written_language, language),
-                        stargazersCount = stargazersCount,
-                        watchersCount = watchersCount,
-                        forksCount = forksCount,
-                        openIssuesCount = openIssuesCount
-                    )
-                )
-            }
-            _repositories.postValue(repositories)
+            GithubApi.searchRepositories(inputText, _repositories)
         }
     }
 }
